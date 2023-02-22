f875b173e71e0c0637af39bf77fffb46596db4e6
Zizzler-dev <113946881+Zizzler-dev@users.noreply.github.com>
2023-02-22 10:50:28 -0600

    Update FormatChangeScript.py
    csv output change

--- FormatChangeScript.py
+++ FormatChangeScript.py
@@ -214,7 +214,7 @@ if csv is not None:
         st.download_button(
             label = "Download data as CSV",
             data = csv_df.to_csv(index = False).encode('utf-8'),
-            file_name = 'zizzl_MMDDYYYY_125.EL1',
+            file_name = 'zizzl_MMDDYYYY_125.csv',
             mime='text'
         )
     
@@ -279,7 +279,7 @@ if csv is not None:
         st.download_button(
             label = "Download data as CSV",
             data = csv_df.to_csv(index = False).encode('utf-8'),
-            file_name = 'zizzl_MMDDYYYY_105.HFI',
+            file_name = 'zizzl_MMDDYYYY_105.csv',
             mime='text'
         )
     
@@ -423,7 +423,7 @@ if csv is not None:
         st.download_button(
             label = "Download data as CSV",
             data = csv_df.to_csv(index = False).encode('utf-8'),
-            file_name = 'zizzl_MMDDYYYY_105.EL1',
+            file_name = 'zizzl_MMDDYYYY_105.csv',
             mime='text'
         )
 
@@ -553,6 +553,6 @@ if csv is not None:
         st.download_button(
             label = "Download data as CSV",
             data = csv_df.to_csv(index = False).encode('utf-8'),
-            file_name = 'zizzl_MMDDYYYY_223.EL1',
+            file_name = 'zizzl_MMDDYYYY_223.csv',
             mime='text'
         )
